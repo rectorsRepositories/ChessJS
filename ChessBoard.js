87b4cfee7c85f8f786fcc699c43760db2cabadcc
armadilla <armadillofast@gmail.com>
2025-08-25 10:50:05 -0400

    ChessBoard.initChessBoardArr() initializes ChessBoard.chessBoard arr with BoardSquares

--- ChessBoard.js
+++ ChessBoard.js
@@ -1,10 +1,13 @@
 class ChessBoard {
   constructor() {
     this.chessBoard = [[], [], [], [], [], [], [], []];
+    this.rankWidth = 50;
+    this.fileHeight = 50;
   }
-  drawBackground() {
-    let cellSize = width / 8;
 
+  drawChessBoard() {
+    let cellSize = (width - 50) / 8;
+    //draw background squares
     for (let row = 0; row < 8; row++) {
       for (let col = 0; col < 8; col++) {
         if ((row + col) % 2 === 0) {
@@ -13,8 +16,20 @@ class ChessBoard {
           fill("#000103"); // dark square
         }
         noStroke();
-        square(col * cellSize, row * cellSize, cellSize);
+        square(col * cellSize + 50, row * cellSize, cellSize);
       }
     }
+    //draw rank and file
+  }
+  initChessBoardArr() {
+    let cellSize = (width - 50) / 8;
+    let middleCellCoordOffset = cellSize / 2;
+    for (let row = 0; row < 8; row++) {
+      for (let col = 0; col < 8; col++) {
+        let newSquare = new BoardSquare((col * cellSize) + middleCellCoordOffset, (row * cellSize) + middleCellCoordOffset, "", "");
+        this.chessBoard[row].push(newSquare);
+      }
+    }
+    console.log(this.chessBoard);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-
 const chessBoard = new ChessBoard();
 function setup() {
   createCanvas(800, 800);
+  chessBoard.initChessBoardArr();
 }
 
 function draw() {
   background(220);
-  chessBoard.drawBackground();
+  chessBoard.drawChessBoard();
 }
