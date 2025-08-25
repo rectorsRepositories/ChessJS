f2343cccbd6e8003a6e25c0f940f5cab539a1e9d
armadilla <armadillofast@gmail.com>
2025-08-25 14:00:58 -0400

    ChessBoard.drawChessBoard draws square files onto canvas

--- ChessBoard.js
+++ ChessBoard.js
@@ -33,20 +33,51 @@ class ChessBoard {
       }
     }
     noFill();
-    //draw rank
+    //draw rank (up/down)
     for (let i = 0; i <= 8; i++) {
       if (i < 8) {
-        stroke("#000103");
-        fill("yellow");
-        rect(0, cellSize * i, rankWidth, cellSize);
         fill("purple");
         textSize(rankWidth / 2);
         text(i + 1, rankWidth / 2, cellSize * i + cellSize / 2);
+        noFill();
       }
     }
-    //draw file
-    for (let i = 0; i <= 8; i++) {
-      
+    //draw file (left/right)
+    for (let i = 0; i < 8; i++) {
+      let fileNum = i + 1;
+      let fileChar
+      switch (fileNum) {
+        case 1:
+          fileChar = "A";
+          break;
+        case 2:
+          fileChar = "B";
+          break;
+        case 3:
+          fileChar = "C";
+          break;
+        case 4: 
+          fileChar = "D";
+          break;
+        case 5:
+          fileChar = "E";
+          break;
+        case 6:
+          fileChar = "F";
+          break;
+        case 7:
+          fileChar = "G";
+          break;
+        case 8:
+          fileChar = "H";
+          break;
+        default:
+          break;
+      }
+      fill("purple");
+      textSize(rankWidth / 2)
+      text(fileChar, i * cellSize + rankWidth + cellSize / 2 , cellSize * 8 + cellSize / 2)
+      noFill();
     }
   }
   initChessBoardArr() {
